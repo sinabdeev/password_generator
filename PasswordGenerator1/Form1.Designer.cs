17bcf1b021c990e737e738233cba01e9e5027cde
vitaliy.sinab@instinc.tools <vitaliy.sinab@instinc.tools>
2017-09-20 09:26:27 +0400

    first commit

---
+++ PasswordGenerator1/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace PasswordGenerator1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkBoxNumbers = new System.Windows.Forms.CheckBox();
+            this.checkBoxUpper = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox1.Location = new System.Drawing.Point(12, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1019, 80);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(329, 128);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(702, 96);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "gener";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(13, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 24);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "7";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(13, 193);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(208, 31);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "copy to buffer";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkBoxNumbers
+            // 
+            this.checkBoxNumbers.AutoSize = true;
+            this.checkBoxNumbers.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxNumbers.Location = new System.Drawing.Point(121, 12);
+            this.checkBoxNumbers.Name = "checkBoxNumbers";
+            this.checkBoxNumbers.Size = new System.Drawing.Size(88, 22);
+            this.checkBoxNumbers.TabIndex = 5;
+            this.checkBoxNumbers.Text = "Numbers";
+            this.checkBoxNumbers.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxUpper
+            // 
+            this.checkBoxUpper.AutoSize = true;
+            this.checkBoxUpper.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.checkBoxUpper.Location = new System.Drawing.Point(226, 12);
+            this.checkBoxUpper.Name = "checkBoxUpper";
+            this.checkBoxUpper.Size = new System.Drawing.Size(67, 22);
+            this.checkBoxUpper.TabIndex = 6;
+            this.checkBoxUpper.Text = "Upper";
+            this.checkBoxUpper.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1044, 235);
+            this.Controls.Add(this.checkBoxUpper);
+            this.Controls.Add(this.checkBoxNumbers);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "PasswordGenerator1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckBox checkBoxNumbers;
+        private System.Windows.Forms.CheckBox checkBoxUpper;
+    }
+}
+
---
+++ PasswordGenerator1/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace PasswordGenerator1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private string getLowChars()
+        {
+            string chars1 =
+                "abdefghijkmnpstuvwxyz";
+
+            chars1 = chars1.Replace("o", "");
+            chars1 = chars1.Replace("c", "");
+            chars1 = chars1.Replace("q", "");
+            return chars1;
+        }
+
+        private string getUpChars()
+        {
+            string chars2 =
+            "ABDEFGHJKLMNPQRSTUVWXYZ";
+
+            chars2 = chars2.Replace("O", "");
+            chars2 = chars2.Replace("C", "");
+            chars2 = chars2.Replace("I", "");
+
+            return chars2;
+        }
+
+        private string getNumbers()
+        {
+            string numbers = "123456789";
+            numbers = numbers.Replace("0", "");
+
+            string result = "";
+            result = numbers ; 
+
+            return result;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string symbolsAndchars = getLowChars();
+                if (this.checkBoxNumbers.Checked == true)
+                {
+                    symbolsAndchars = symbolsAndchars + getNumbers();
+                }
+                if (checkBoxUpper.Checked == true) 
+                {
+                    symbolsAndchars = symbolsAndchars + getUpChars();
+                }
+                int passwordLength = Convert.ToInt32(textBox2.Text);
+                string password = "";
+                int seed = Convert.ToInt32(Math.Sqrt(DateTime.Now.Ticks));
+                Random r = new Random();
+                for (int i = 0; i < passwordLength; i++)
+                {
+                    int j = r.Next(0, symbolsAndchars.Length);
+                    string letter = symbolsAndchars[j].ToString();
+                    password = password + letter;
+                }
+                textBox1.Text = password;
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Clipboard.SetText(textBox1.Text); 
+        }
+
+    }
+}
